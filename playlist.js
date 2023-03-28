088bc418f9d77855de61f3dda972606d74a40df5
osboxes.org <milktealliance@protonmail.com>
2023-03-28 19:47:43 +0100

    upload all files

---
+++ playlist.js
@@ -0,0 +1,84 @@
+require('dotenv').config()
+const axios = require('axios')
+const base64 = require('base-64')
+
+
+
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+
+async function getToken() {
+  const authString = `${CLIENT_ID}:${CLIENT_SECRET}`
+  const authBase64 = base64.encode(authString)
+
+  const headers = {
+    'Authorization': `Basic ${authBase64}`,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  const data = 'grant_type=client_credentials'
+
+  const response = await axios.post('https://accounts.spotify.com/api/token', data, {
+    headers: headers
+  })
+
+  const token = response.data.access_token
+  return token
+}
+var artistName ='X-Japan'
+async function searchForArtist(token, artistName) {
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const query = `q=${artistName}&type=artist&limit=1`
+  const url = `https://api.spotify.com/v1/search?${query}`
+
+  const response = await axios.get(url, {
+    headers: headers
+  })
+
+  const artist = response.data.artists.items[0]
+  if (!artist) {
+    console.log('No artist found')
+    return null
+  }
+
+  return artist
+}
+
+async function getSongsByArtist(token, artistId) {
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const market = 'JP'
+  const seed_genres ='j-pop'
+  const valence = '0.7'
+  const url = `https://api.spotify.com/v1/recommendations?limit=15&market=${market}&seed_genres=${seed_genres}&target_valence=${valence}`
+
+  const response = await axios.get(url, {
+    headers: headers
+  })
+
+  const songs = response.data.tracks
+  return songs
+}
+
+async function main() {
+  const token = await getToken()
+  const artist = await searchForArtist(token, artistName)
+
+  if (!artist) {
+    return
+  }
+
+  const songs = await getSongsByArtist(token, artist.id)
+  songs.forEach((song, idx) => {
+    console.log(`${idx+1}. ${song.name} by ${song.artists[0].name}`)
+
+  })
+  console.log(`This is my token: ${token}`)
+}
+
+main()
